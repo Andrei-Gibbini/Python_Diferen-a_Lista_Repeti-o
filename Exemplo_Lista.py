1068aa6fd78c82e2b8b86caf95c2c4f89219b0be
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-04-25 09:15:49 -0300

    Alterado o formato do Exemplo 1 para o formato de lista com laço de repetição.

---
+++ Exemplo_Lista.py
@@ -0,0 +1,32 @@
+'''
+Crie um código para ler e calcular a média salarial inserida e apontar os salários menores do que a média
+'''
+
+salario = [0,0,0,0]
+
+soma = 0
+salario[0] = float(input('Salário R$: '))
+soma += salario[0]
+salario[1] = float(input('Salário R$: '))
+soma += salario[1]
+salario[2] = float(input('Salário R$: '))
+soma += salario[2]
+salario[3] = float(input('Salário R$: '))
+soma += salario[3]
+
+media = soma/4
+print(f'media salarial: {media:.2f}')
+
+if salario[0] < media:
+    print(f'Salário abaixo da média: {salario[0]:.2f}')
+if salario[1] < media:
+    print(f'Salário abaixo da média: {salario[1]:.2f}')
+if salario[2] < media:
+    print(f'Salário abaixo da média: {salario[2]:.2f}')
+if salario[3] < media:
+    print(f'Salário abaixo da média: {salario[3]:.2f}')
+
+'''
+Esse formato é o correto, deve-se usar lista com laço de repetição
+Facilita muito a criação e execução
+'''
