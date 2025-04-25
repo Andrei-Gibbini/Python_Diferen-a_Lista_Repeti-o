1accd0616e1eb24a8dd6598468c2b667424a3f45
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-04-25 08:59:13 -0300

    Criado um código em Python para calcular a média salarial de 4 valores ( indo de 0 a 3) e apresentar aqueles que estão abaixo da média.

---
+++ Exemplo 1.py
@@ -0,0 +1,21 @@
+soma = 0
+salario_0 = float(input('Salário R$: '))
+soma += salario_0
+salario_1 = float(input('Salário R$: '))
+soma += salario_1
+salario_2 = float(input('Salário R$: '))
+soma += salario_2
+salario_3 = float(input('Salário R$: '))
+soma += salario_3
+
+media = soma/4
+print(f'media salarial: {media:.2f}')
+
+if salario_0 < media:
+    print(f'Salário abaixo da média: {salario_0:.2f}')
+if salario_1 < media:
+    print(f'Salário abaixo da média: {salario_1:.2f}')
+if salario_2 < media:
+    print(f'Salário abaixo da média: {salario_2:.2f}')
+if salario_3 < media:
+    print(f'Salário abaixo da média: {salario_3:.2f}')
